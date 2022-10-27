0736ba16760be3077969f9ac0eee5a0fc8dbdc4b
kiaisoft.anh.luu <kiaisoft.anh.luu@gmail.com>
2022-10-28 00:51:45 +0700

    fix Link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Button() {
     return (
-        <button className="text-black lg:px-8 lg:py-[18px] px-5 py-4 btnBackGround font-medium hover:scale-105 transition delay-300 duration-300 ease-in-out">
-            Get Started
-        </button>
+        <Link to="/">
+            <button className="text-black lg:px-8 lg:py-[18px] px-5 py-4 btnBackGround font-medium hover:scale-105 transition delay-300 duration-300 ease-in-out">
+                Get Started
+            </button>
+        </Link>
     );
 }
 
